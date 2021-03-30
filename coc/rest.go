ed9cc5f17a506116fbd3e4f9185cef484721979f
Clash Go <clashgolang@gmail.com>
2021-03-30 17:40:06 -0400

    Initial

--- coc/rest.go
+++ coc/rest.go
@@ -8,17 +8,17 @@ var (
 	defaultHeaders = rest.Headers{
 		"Accept": "application/json",
 	}
-	Token string
+	token string
 )
 
 // SetToken sets the token to be used on requests sent to Clash of Clans
 func SetToken(t string) {
-	Token = t
+	token = t
 }
 
 // get retrieves the requested URL and return the results as a byte array.
 func get(url string, qparms rest.QParms) ([]byte, error) {
-	headers := rest.Headers{"Authorization": "Bearer " + Token}
+	headers := rest.Headers{"Authorization": "Bearer " + token}
 	for k, v := range defaultHeaders {
 		headers[k] = v
 	}
